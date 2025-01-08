30369a85b13d48ed901d4e65202ab8e65a9e506e
Nikola P. <nikoladevelops@gmail.com>
2025-01-08 17:00:51 +0200

    Add code related to database connection and generation

---
+++ LibraryWebApp/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LibraryWebApp.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        // TODO add aditional properties that each user should possess
+    }
+}
--- LibraryWebApp/Program.cs
+++ LibraryWebApp/Program.cs
@@ -1,9 +1,35 @@
+using LibraryWebApp.Models;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Read the .env file in the project directory, automatically adds all those key value pairs as environment variables that can be accessed in runtime (you should create that file and have that DotNetEnv package as I mentioned in the README.md)
+DotNetEnv.Env.Load();
+
+string? connection_string = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+if (connection_string == null) 
+{
+    throw new Exception("You haven't configured the connection string, check Program.cs and the .env file");
+}
+
+// Sets up the db context, so now we can communicate with the database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connection_string));
+
+
+// Add Identity support + tables
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+// Build the app
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,6 +44,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Makes it possible to validate requests / who the user is /  checking for a valid token or cookie (the client's details are saved into User.Identity.Name, User.Claims)
+app.UseAuthentication();
+
+// Based on the previous Authentication user details, checks if the user has the valid Roles / Claims / Policies (basically allows attributes like these to work: [Authorize(Roles = "Admin")] )
 app.UseAuthorization();
 
 app.MapControllerRoute(
