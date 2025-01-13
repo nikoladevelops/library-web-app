2d0551dffcf90ec3aac71b71f38f7b3b268a5137
MomchilSimeonov <frozer@abv.bg>
2025-01-13 16:38:36 +0200

    created login page
    Created login page for users and adjusted main menu to be logically sound

---
+++ LibraryWebApp/Controllers/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using LibraryWebApp.Models;
+using LibraryWebApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        public LoginController( SignInManager<ApplicationUser> signIn, UserManager<ApplicationUser> um)
+        {
+            signInManager = signIn;
+            userManager = um;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = await signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
+
+                if (res.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = registerViewModel.Username,
+                    Email = registerViewModel.Email
+                };
+                await userManager.CreateAsync(user, registerViewModel.Password);
+                //redirect to book panel here
+                return RedirectToAction(nameof(Index));
+            }
+        return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Index)); ;
+        }
+    }
+}
---
+++ LibraryWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+
+        public string Username { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).*$", ErrorMessage = "Password must have at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword", ErrorMessage = "Password must match")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
