c128e625873273eb444228b32a0f94bf8c911922
Nikich <75580734+nikoladevelops@users.noreply.github.com>
2025-01-14 13:09:05 +0200

    Add book cover image support (#24)
    Created BookCoverImageManager. Closes #5, #24

--- LibraryWebApp/Controllers/BookController.cs
+++ LibraryWebApp/Controllers/BookController.cs
@@ -7,16 +7,18 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LibraryWebApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using LibraryWebApp.Helpers;
 
 namespace LibraryWebApp.Controllers
 {
     public class BookController : Controller
     {
         private readonly ApplicationDbContext _context;
-
-        public BookController(ApplicationDbContext context)
+        private readonly IBookCoverImageManager _bookCoverImageManager;
+        public BookController(ApplicationDbContext context, IBookCoverImageManager imageSaver)
         {
             _context = context;
+            _bookCoverImageManager = imageSaver;
         }
 
         public async Task<IActionResult> Index()
@@ -40,6 +42,9 @@ namespace LibraryWebApp.Controllers
                 return NotFound();
             }
 
+            // TODO should use viewmodels instead
+            ViewData["NoBookCoverImage"] = _bookCoverImageManager.DefaultNoBookCoverImagePath;
+
             return View(book);
         }
 
@@ -53,12 +58,14 @@ namespace LibraryWebApp.Controllers
         // TODO Make it accessible to Admins only
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Book book) // TODO should probrably switch to using ViewModels
+        public async Task<IActionResult> Create(Book book, IFormFile? coverImage) // TODO should probably switch to using ViewModels
         {
             ValidatePublicationDate(book.PublicationDate);
 
             if (ModelState.IsValid)
             {
+                await SaveBookCoverImage(book, coverImage);
+
                 book.AvailableCount = book.TotalCount;
                 _context.Add(book);
                 await _context.SaveChangesAsync();
@@ -80,23 +87,44 @@ namespace LibraryWebApp.Controllers
             {
                 return NotFound();
             }
+
             return View(book);
         }
 
         // TODO Make it accessible to Admins only
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Book book)
+        public async Task<IActionResult> Edit(Book book, IFormFile? coverImage)
         {
+            var bookInDb = await _context.Books.FindAsync(book.Id);
+
+            if (bookInDb == null)
+            {
+                return NotFound();
+            }
+            
+
             ValidatePublicationDate(book.PublicationDate);
 
             if (ModelState.IsValid)
             {
-                _context.Update(book);
+                // If the book had a cover image already saved in the database, delete it
+                if (bookInDb.CoverImageUrl != null) 
+                {
+                    // Deletes the actual file
+                    _bookCoverImageManager.DeleteBookCoverImage(bookInDb.CoverImageUrl);
+                }
+
+                // Now book has no bookInDbCoverImageUrl which is good.
+
+                _context.Entry(bookInDb).CurrentValues.SetValues(book); // This also copied the old book cover image url
+
+                await SaveBookCoverImage(bookInDb, coverImage);
                 await _context.SaveChangesAsync();
                
                 return RedirectToAction(nameof(Index));
             }
+
             return View(book);
         }
 
@@ -116,6 +144,9 @@ namespace LibraryWebApp.Controllers
                 return NotFound();
             }
 
+            // TODO should use viewmodels instead
+            ViewData["NoBookCoverImage"] = _bookCoverImageManager.DefaultNoBookCoverImagePath;
+
             return View(book);
         }
 
@@ -127,6 +158,11 @@ namespace LibraryWebApp.Controllers
             var book = await _context.Books.FindAsync(id);
             if (book != null)
             {
+                if (book.CoverImageUrl != null)
+                {
+                    _bookCoverImageManager.DeleteBookCoverImage(book.CoverImageUrl);
+                }
+
                 _context.Books.Remove(book);
             }
 
@@ -140,6 +176,26 @@ namespace LibraryWebApp.Controllers
             {
                 ModelState.AddModelError("PublicationDate", "The publication date cannot be in the future.");
             }
+        }
+
+        /// <summary>
+        /// Saves the book cover image to the disk if it was provided. Also sets the book's CoverImageUrl property to be consistent with that (either to null or to the actual book cover image url).
+        /// </summary>
+        /// <param name="book"></param>
+        /// <param name="coverImage"></param>
+        /// <returns></returns>
+        private async Task SaveBookCoverImage(Book book, IFormFile? coverImage) 
+        {
+            // If no cover image was actually passed, that means the book should also be consistent with that
+            if (coverImage == null)
+            {
+                book.CoverImageUrl = null; // Important to ensure that the book has no cover image url if coverImage is null
+                return;
+            }
+
+            // If a new book cover image was provided, then save it
+            string filePath = await _bookCoverImageManager.SaveBookCoverImageToDiskAsync(coverImage);
+            book.CoverImageUrl = filePath;
         }
     }
 }
