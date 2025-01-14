7b6461c85406ba72f36f81b281ad6678e1fa0018
MomchilSim <93608222+MomchilSim@users.noreply.github.com>
2025-01-14 14:07:34 +0200

    Add Login/Register functionality (#23)
    Add authentication
    Closes #7 
    
    Co-authored-by: Nikola P. <nikoladevelops@gmail.com>

---
+++ LibraryWebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryWebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [StringLength(30)]
+        [DisplayName("Username")]
+        public string Username { get; set; }
+
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
